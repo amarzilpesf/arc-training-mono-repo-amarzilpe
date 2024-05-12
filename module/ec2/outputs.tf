dfe45557b25196e95d2d416bc0aba02835985280
amarzilpesf <amar.zilpe@sourcefuse.com>
2024-05-12 19:33:36 +0530

    output fix

--- module/ec2/outputs.tf
+++ module/ec2/outputs.tf
@@ -1,8 +1,3 @@
-output "azs" {
-  description = "Get the list of availability zones in a Region"
-  value       = data.aws_availability_zones.azs.names
-}
-
 output "instance_public_ip" {
   description = "Get instance Public Ip's"
   value       = { for ipi, instance in aws_instance.web : ipi => instance.public_ip }
@@ -10,5 +5,5 @@ output "instance_public_ip" {
 
 output "instance_ids" {
   description = "Get EC2 instance ids"
-  value       = { for id, instance in aws_instance.web : id => instance.public_ip }
+  value       = { for id, instance in aws_instance.web : id => instance.id }
 }
