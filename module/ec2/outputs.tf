261e0cd1bc9dcaa027e12551ef82a8ac1d51433a
amarzilpesf <amar.zilpe@sourcefuse.com>
2024-05-14 13:35:38 +0530

    removed ec2 vpc module

--- module/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Get instance Public Ip's"
-  value       = { for ipi, instance in aws_instance.web : ipi => instance.public_ip }
-}
-
-output "instance_ids" {
-  description = "Get EC2 instance ids"
-  value       = { for id, instance in aws_instance.web : id => instance.id }
-}
--- module/ec2/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_type" {
-  description = "Choose instance type"
-  type        = string
-  default     = "t2.micro"
-}
