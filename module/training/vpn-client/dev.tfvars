0ca0534d5fafdfcd28e3ae7a2c78a015d7309144
amarzilpesf <amar.zilpe@sourcefuse.com>
2024-07-10 18:51:09 +0530

    vpn module

---
+++ module/training/vpn-client/dev.tfvars
@@ -0,0 +1,5 @@
+region                        = "us-east-1"
+environment                   = "dev"
+namespace                     = "arc"
+vpc_name_override             = "arc-dev-vpc"
+private_subnet_names_override = ["arc-dev-private-subnet-private-us-east-1a", "arc-dev-private-subnet-private-us-east-1b"]
---
+++ module/training/vpn-client/variables.tf
@@ -0,0 +1,49 @@
+################################################################################
+## shared
+################################################################################
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace to assign the resources"
+  default     = "arc"
+}
+
+variable "private_subnet_names_override" {
+  type        = list(string)
+  description = "The name of the subnets to associate to the VPN."
+  default     = []
+}
+
+################################################################################
+## certs
+################################################################################
+variable "secret_path_format" {
+  description = "The path format to use when writing secrets to the certificate backend."
+  type        = string
+  default     = "/%s.%s"
+
+  validation {
+    condition     = can(substr(var.secret_path_format, 0, 1) == "/")
+    error_message = "The secret path format must contain a leading slash."
+  }
+}
+
+################################################################################
+## vpn
+################################################################################
+variable "environment" {
+  description = "Name of the environment the resource belongs to."
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Name of the project the vpn resource belongs to."
+  type        = string
+  default     = "arc-iac-amar"
+}
